f8dfeecda9783a35c159760d3059735dfec0a50d
Dmitriy Teterin <teterinda@gmail.com>
2021-11-02 12:02:59 +0300

    Основная стека переведена на Grid. Добавлены favicon и общий логотип проекта

--- webpage/src/components/page-header/page-header.jsx
+++ webpage/src/components/page-header/page-header.jsx
@@ -5,7 +5,8 @@ const PageHeader = () => (
     <div className="container">
       <div className="page-header__wrapper">
         <a className="page-header__logo" href="/">
-          {/* <img className="page-header__logo-image" src="img/icon-logo.svg" width="120" height="40" alt="RecycleID logo" /> */}
+          <img className="page-header__logo-image" src="/icon-logo.svg" width="120" height="40" alt="SmartHome logo" />
+          <span className="page-header__logo-title">SmartHome</span>
         </a>
       </div>
     </div>
