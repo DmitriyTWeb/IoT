954795d181a4c5523c58110b0323f31e9103336c
Dmitriy Teterin <teterinda@gmail.com>
2021-11-03 16:48:37 +0300

    Для оптимизации в качестве favicon и лого использован один файл

--- webpage/src/components/page-header/page-header.jsx
+++ webpage/src/components/page-header/page-header.jsx
@@ -5,7 +5,7 @@ const PageHeader = () => (
     <div className="container">
       <div className="page-header__wrapper">
         <a className="page-header__logo" href="/">
-          <img className="page-header__logo-image" src="/icon-logo.svg" width="120" height="40" alt="SmartHome logo" />
+          <img className="page-header__logo-image" src="/favicon.svg" width="50" height="50" alt="SmartHome logo" />
           <span className="page-header__logo-title">SmartHome</span>
         </a>
       </div>
