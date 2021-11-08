82b8685c52168518a75d08b8fd2f1646693d2b19
Dmitriy Teterin <teterinda@gmail.com>
2021-11-08 22:17:45 +0300

    StateBoard - стилизация

--- webpage/src/components/pump-state/pump-state.jsx
+++ webpage/src/components/pump-state/pump-state.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const PumpState = ({pumpState}) => {
+  const label = pumpState ? 'В РАБОТЕ' : 'ВЫКЛЮЧЕН';
+  const classList = `pump-state__value ${pumpState ? "pump-state__value--on" : ""}`;
+
   return(
     <>
-      <p className="pump-stat__title">Статус включения насоса</p>
-      <p className="pump-stat__value">{pumpState}</p>
+      <p className="pump-state__title">Статус включения насоса</p>
+      <p className={classList}>{label}</p>
     </>
   );
 };
--- webpage/src/components/sensor-status/sensor-status.jsx
+++ webpage/src/components/sensor-status/sensor-status.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 const SensorStatus = ({sensorStatus}) => {
-  console.log(sensorStatus);
+  const isBroken = sensorStatus === 'SENSOR_BROKEN'
+  const label =  isBroken ? 'ОБРЫВ' : 'В НОРМЕ';
+  const classList = `sensor-status__value ${isBroken ? "sensor-status__value--broken" : ""}`;
+
   return(
     <>
       <p className="sensor-status__title">Статус датчика</p>
-      <p className="sensor-status__value">{sensorStatus}</p>
+      <p className={classList}>{label}</p>
     </>
 
   );
