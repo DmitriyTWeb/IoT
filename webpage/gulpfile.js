ece623119015090b7461c2e16880374f1a1300a5
Dmitriy Teterin <teterinda@gmail.com>
2021-11-09 13:27:59 +0300

    Обновление автоматизации gulpfile.js

--- webpage/gulpfile.js
+++ webpage/gulpfile.js
@@ -46,6 +46,8 @@ gulp.task("server", function () {
   gulp.watch("src/components/**/*.jsx", gulp.series("webpack", "refresh"));
   gulp.watch("src/hooks/*.js", gulp.series("webpack", "refresh"));
   gulp.watch("src/*.js", gulp.series("webpack", "refresh"));
+  gulp.watch("src/*.tsx", gulp.series("webpack", "refresh"));
+  gulp.watch("src/services/*.js", gulp.series("webpack", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
