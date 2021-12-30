3b89b53a2bcfa87d6678d8b20fa9beba6fa63a58
DTeterin <teterinda@gmail.com>
2021-12-30 14:15:57 +0300

    fixup! StateBoard - переход на tailwindcss

--- webpage/src/components/state-board/state-board.tsx
+++ webpage/src/components/state-board/state-board.tsx
@@ -8,8 +8,8 @@ import { DeviceStateProps } from "../../store/reducers/state-reducer";
 
 const StateBoard: FC<DeviceStateProps> = ({ currentTemp, sensorStatus, pumpState }) => {
   return(
-    <section className="relative grid grid-cols-[2fr_1fr] mt-20 xl:mt-30 p-4 border border-dashed rounded-2xl state">
-      <h1 className="state-title">Состояние системы</h1>
+    <section className="relative grid grid-cols-[2fr_1fr] mt-20 xl:mt-30 mb-8 md:mb-12 p-4 border border-dashed border-slate-600 rounded-md gap-y-2 md:gap-y-4">
+      <h1 className="absolute bg-white -top-4 left-4 px-2 text-lg text-slate-500">Состояние системы</h1>
       <FloorTemp floorTemp={currentTemp} />
       <SensorStatus sensorStatus={sensorStatus} />
       <PumpState pumpState={pumpState}/>
